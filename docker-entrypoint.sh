de236d4e18be4cb63d1f6540da64183b6eed9d42
ipp-docker-hub-client <ipp-docker-hub-client@users.noreply.github.com>
2016-08-23 14:25:41 +0800

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+if [[ "$1" == nginx ]] || [ "$1" == php-fpm ]; 
+then
+  chown -R 0:0 /var/www/html
+  echo "Running PHP-FPM ..."
+  php-fpm --allow-to-run-as-root --nodaemonize &
+  echo "Running Nginx ..."
+  nginx -g 'daemon off;'
+else
+  exec "$@"
+fi
