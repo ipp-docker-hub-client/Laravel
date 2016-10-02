a80addef7588b0bbfd2f985484791e77ac3170ef
ipp-docker-hub-client <ipp-docker-hub-client@users.noreply.github.com>
2016-10-02 17:37:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,23 +9,24 @@ then
     if [ "${CAENV}" = "production" ]
     then 
       mv .env.production .env
+      echo "Production configs are set."
     elif [ "${CAENV}" = "staging" ]
     then 
       mv .env.staging .env
+      echo "Staging configs are set."
     else
       echo "Environment veriable is not set! Task aborted."
     fi
-  echo "Done."
   echo "Setting up Newrelic configs ..."
     if [ "${NEWRELIC_LICENSE}" != "**None**" ]
     then
       sed -i "s/newrelic.enabled = false/newrelic.enabled = true/g" /usr/local/etc/php/conf.d/newrelic.ini
       sed -i "s/NRKEY/"${NEWRELIC_LICENSE}"/g" /usr/local/etc/php/conf.d/newrelic.ini
       sed -i 's/NRNAME/"${NEWRELIC_APPNAME}"/g' /usr/local/etc/php/conf.d/newrelic.ini
+      echo "Newrelic configs are set."
     else
       echo "No Newrelic license found! Task aborted."
     fi
-  echo "Done."
   echo "Running PHP-FPM ..."
     php-fpm --allow-to-run-as-root --nodaemonize &
   echo "Running Nginx ..."
